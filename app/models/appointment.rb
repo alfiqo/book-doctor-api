bb74d6744fcf9d8b9f5b8ace8c472343130fe631
Alfiqo Jundullah <phicko93@gmail.com>
2020-10-21 00:38:56 +0700

    validate cant book before schedule start

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,11 +2,17 @@ class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :schedule
   validates_presence_of :queue, :user, :schedule
-  after_validation :validate_schedule_max
+  after_validation :validate_schedule_max, :validate_schedule_time
 
   private
 
   def validate_schedule_max
     errors.add(:schedule_id, "fully booked.") if self.schedule.appointments.count >= 10
+  end
+
+  def validate_schedule_time
+    start = Time.parse((self.schedule.schedule_start - 30.minutes).strftime("%H:%M:%S"))
+    current = Time.parse((Time.now).strftime("%H:%M:%S"))
+    errors.add(:schedule_id, "can't book within 30 minutes before the doctor starts the schedule.") unless (current < start)
   end
 end
